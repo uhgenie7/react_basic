657dc00e86e16146a90e391abc305e58a422faba
hyejin <dzzd777@naver.com>
2021-04-06 14:50:56 +0900

    [Update] 컴포넌트 정리

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,12 +5,25 @@ import PropTypes from 'prop-types';
 //   return <h4>Title</h4>
 // }
 
-function Movie({title, year, rating}) {
+function Movie({title, year, summary, poster, genres}) {
   return (
-    <div>
-      <h4>{title}</h4>
-      <h3>{year}</h3>
-      <em>{rating}</em>
+    <div className="movie">
+      <img src={poster} alt={title}></img>
+      <div className="movie_data">
+        <h3 className="movie_title">{title}</h3>
+        <h5 className="movie_year">{year}</h5>
+        <ul>
+          {genres.map((genres, idx) => {
+            return (
+              <li key={idx}>{genres}</li>
+            )
+          })}
+          {/* map은 return이 있으면 {}, 없으면 ()을 쓴다 */}
+        </ul>
+        <p className="movie_summery">{ summary}</p>
+      </div>
+
+
     </div>
     )
 }
@@ -18,7 +31,9 @@ function Movie({title, year, rating}) {
 Movie.propType = {
   title: PropTypes.string.isRequired,
   year: PropTypes.number.isRequired,
-  rating: PropTypes.number.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default Movie;
